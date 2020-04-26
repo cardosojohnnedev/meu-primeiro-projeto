9f3f13d9190e3f8c0e6b21202c1910ed690ab139
João <contato.joaocardoso@icloud.com>
2020-04-25 23:45:06 -0300

    instalação do Bower e suas dependencias mais inclusão de arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-
 //Task para o sass
 gulp.task('sass', function(){
-    return gulp.src('sass/*.sass')
-    .pipe(sass().on('erro', sass.logError))
+    return gulp.src('sass/**/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('erro', sass.logError))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('sass:watch', function(){
-    gulp.watch('sass/*.sass',['sass']);
-});
+gulp.task('watch', gulp.series('sass', function (){ 
+    gulp.watch('sass/**/*.sass', gulp.series('sass')); 
+   }));
+
+gulp.task('default', gulp.series('sass', 'watch'));
+
+//gulp.task('sass:watch', function(){
+  //  gulp.watch('sass/**/*.sass',['sass']);
+//});
 
 //task default gulp
-gulp.task('default', function(){
-    
-});
+//gulp.task('default', ['sass','watch']);
